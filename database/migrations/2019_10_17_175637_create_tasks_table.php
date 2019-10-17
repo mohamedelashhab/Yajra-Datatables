07a054c8edd2da2cfe0784d52305d014737e15f3
ashhab <elashhab_fcih@yahoo.com>
2019-10-17 20:08:58 +0200

    task table migration

---
+++ database/migrations/2019_10_17_175637_create_tasks_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    //(id,name,image,screen_name,content,description,user_name,date,statuse 
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 50);
+            $table->string('image');
+            $table->string('screen_name', 50);
+            $table->text('content');
+            $table->text('description');
+            $table->string('user_name');
+            $table->string('statuse');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
